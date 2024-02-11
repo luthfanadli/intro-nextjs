cf06d7ee1ccddd7ffb7facd16abf9e455471ffaa
luthfanadli <luthfan30@gmail.com>
2024-02-11 23:39:58 +0700

    feat: static and dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
           />
         </div>
       </div>
-      <div className={styles.shape} />
+      {/* <div className={styles.shape} /> */}
     </main>
   );
 }
